66329cd86dae0eed7451816136a7080ef9be72d3
Mariane Requinton <200610149@my.xu.edu.ph>
2023-05-15 01:26:19 +0800

    added codebox + twillio compoenent placeholder

---
+++ ui/src/components/TwillioNotif.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+      <label for="to">To:</label>
+      <input type="tel" id="to" v-model="toNumber">
+      <br>
+      <label for="message">Message:</label>
+      <textarea id="message" v-model="message"></textarea>
+      <br>
+      <button @click="send">Send Message</button>
+    </div>
+  </template>
+  
+<script>
+import axios from 'axios';
+  
+  export default {
+    name: 'TwillioNotif',
+    data() {
+      return {
+        toNumber: '',
+        message: ''
+      };
+    },
+    methods: {
+      async send() {
+        const accountSid = 'YOUR_TWILIO_ACCOUNT_SID_HERE';
+        const authToken = 'YOUR_TWILIO_AUTH_TOKEN_HERE';
+        const twilioNumber = 'YOUR_TWILIO_PHONE_NUMBER_HERE';
+  
+        try {
+          const response = await axios.post(`https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`, {
+            To: this.toNumber,
+            From: twilioNumber,
+            Body: this.message
+          }, {
+            auth: {
+              username: accountSid,
+              password: authToken
+            }
+          });
+  
+          console.log(response.data);
+          alert('Message sent successfully!');
+        } catch (error) {
+          console.error(error);
+          alert('Failed to send message');
+        }
+      }
+    }
+  };
+</script>
+  
