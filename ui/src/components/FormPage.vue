d5c9fc2cbd0e1a31afc815d5143e07d30cb66f18
Christianne San Jose <20140006628@my.xu.edu.ph>
2023-05-17 20:18:38 +0800

    modified Form page

--- ui/src/components/FormPage.vue
+++ ui/src/components/FormPage.vue
@@ -3,86 +3,91 @@
         <title>Property Pass Form</title>
     </head>
     <body>
-        <div>
-            <label>Nature of Transaction:</label>
-            <multiselect
-            v-model="selectedTransaction"
-            :options="nature"
-            @input="handleOptionChange"
-            ></multiselect>
+        <div class = "slider-container">
+          <div class="slider-content" :style="{ left: currentPosition }">
+            <div class = "slide">
+              <label>Nature of Transaction:</label>
+              <multiselect
+              v-model="selectedTransaction"
+              :options="nature"
+              @input="handleOptionChange"
+              ></multiselect>
 
-            <div v-if="selectedTransaction === 'Sales(Scraps, MRF, Vermi, Manresa Farm products, etc.)'">
-            <label>Select another option:</label>
-            <label>Official Receipt No.:</label>
-            <input type="text" v-model="sales_official_receipt" />
-            <label>Upload XU Official Receipt:</label>
-            <input type="file" @change="handleFileChange" />
-            </div>
+              <div v-if="selectedTransaction === 'Sales(Scraps, MRF, Vermi, Manresa Farm products, etc.)'">
+              <label>Select another option:</label>
+              <label>Official Receipt No.:</label>
+              <input type="text" v-model="sales_official_receipt" />
+              <label>Upload XU Official Receipt:</label>
+              <input type="file" @change="handleFileChange" />
+              </div>
 
-            <div v-if="selectedTransaction === 'Transfer Location/Property Donation'">
-            <label>From:</label>
-            <input type="text" v-model="transfer_from" />
-            <label>To:</label>
-            <input type="text" v-model="transfer_to" />
-            <label>Transfer Form No.:</label>
-            <input type="text" v-model="transfer_form_number" />
-            <label>Upload Accomplished Transfer Form w/ MR:</label>
-            <input type="file" @change="handleFileChange" />
-            </div>
+              <div v-if="selectedTransaction === 'Transfer Location/Property Donation'">
+              <label>From:</label>
+              <input type="text" v-model="transfer_from" />
+              <label>To:</label>
+              <input type="text" v-model="transfer_to" />
+              <label>Transfer Form No.:</label>
+              <input type="text" v-model="transfer_form_number" />
+              <label>Upload Accomplished Transfer Form w/ MR:</label>
+              <input type="file" @change="handleFileChange" />
+              </div>
 
-            <div v-if="selectedTransaction === 'For Repair/Replacement'">
-            <label>Warranty availability:</label>
-            <multiselect
-                v-model="repair_warranty"
-                :options="repair_or_replacement"
-            ></multiselect>
-            <label>Company:</label>
-            <input type="text" v-model="repair_company" placeholder="Enter company" />
-            <label>Upload Assessment from CISO or PPO:</label>
-            <input type="file" @change="handleFileChange" />
-            </div>
+              <div v-if="selectedTransaction === 'For Repair/Replacement'">
+              <label>Warranty availability:</label>
+              <multiselect
+                  v-model="repair_warranty"
+                  :options="repair_or_replacement"
+              ></multiselect>
+              <label>Company:</label>
+              <input type="text" v-model="repair_company" placeholder="Enter company" />
+              <label>Upload Assessment from CISO or PPO:</label>
+              <input type="file" @change="handleFileChange" />
+              </div>
 
-            <div v-if="selectedTransaction === 'Borrowed items/equipment'">
-            <label>Location:</label>
-            <input type="text" v-model="borrowed_location" placeholder="Enter full address" />
-            <label>Date item returned:</label>
-            <date-pick v-model="borrowed_return_date" />
-            <label>Upload Request to Borrow Form:</label>
-            <input type="file" @change="handleFileChange" />
-            </div>
+              <div v-if="selectedTransaction === 'Borrowed items/equipment'">
+              <label>Location:</label>
+              <input type="text" v-model="borrowed_location" placeholder="Enter full address" />
+              <label>Date item returned:</label>
+              <date-pick v-model="borrowed_return_date" />
+              <label>Upload Request to Borrow Form:</label>
+              <input type="file" @change="handleFileChange" />
+              </div>
 
-            <div v-if="selectedTransaction === 'Others'">
-            <label>Input your nature of transaction:</label>
-            <input type="text" v-model="others_description" placeholder="Enter description" />
+              <div v-if="selectedTransaction === 'Others'">
+              <label>Input your nature of transaction:</label>
+              <input type="text" v-model="others_description" placeholder="Enter description" />
+              </div>
+
+              <button type="next" @click="slideNext">Next</button>
             </div>
+            <div class = "slide">
+              <label>Number of items to process:</label>
+              <input type="number" v-model="number_of_items" />
+              <table>
+                  <thead>
+                      <tr>
+                      <th>Item No.</th>
+                      <th>Particulars</th>
+                      <th>Property Tag</th>
+                      <th>No. of items</th>
+                      <th>Amount</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <tr v-for="(input, index) in inputFields" :key="index">
+                      <td>{{ index + 1 }}</td>
+                      <td><input type="text" v-model="input.particulars" /></td>
+                      <td><input type="text" v-model="input.propertyTag" /></td>
+                      <td><input type="number" v-model="input.numberOfItems" /></td>
+                      <td><input type="number" v-model="input.amount" /></td>
+                      </tr>
+                  </tbody>
+              </table>
+              <button @click="slideBack">Back</button>
+              <button type="submit" @click="saveData">Submit</button>
         </div>
-        <div>
-            <label>Number of items to process:</label>
-            <input type="number" v-model="number_of_items" />
-            <table>
-                <thead>
-                    <tr>
-                    <th>Item No.</th>
-                    <th>Particulars</th>
-                    <th>Property Tag</th>
-                    <th>No. of items</th>
-                    <th>Amount</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(input, index) in inputFields" :key="index">
-                    <td>{{ index + 1 }}</td>
-                    <td><input type="text" v-model="input.particulars" /></td>
-                    <td><input type="text" v-model="input.propertyTag" /></td>
-                    <td><input type="number" v-model="input.numberOfItems" /></td>
-                    <td><input type="number" v-model="input.amount" /></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        <div>
-          <button type="submit" @click="saveData">Submit</button>
-        </div>
+       </div>
+       </div>
     </body>
 </template>
 
@@ -114,6 +119,9 @@ export default {
       nature: ['Sales(Scraps, MRF, Vermi, Manresa Farm products, etc.)', 'Transfer Location/Property Donation',
                 'For Repair/Replacement', 'Borrowed items/equipment', 'Others'],
       repair_or_replacement: ['With Warranty', 'Without Warranty'],
+
+      currentPosition: "0px",
+      slideWidth: 0,
     };
   },
   methods: {
@@ -144,6 +152,23 @@ export default {
 
     handleFileChange(event) {
       this.file = event.target.files[0];
+    },
+
+    slideNext() {
+      const numSlides = document.querySelectorAll(".slide").length;
+      const maxWidth = this.slideWidth * (numSlides - 1);
+      const currentPositionValue = parseInt(this.currentPosition, 10);
+
+      if (currentPositionValue > -maxWidth) {
+        this.currentPosition = `${currentPositionValue - this.slideWidth}px`;
+      }
+    },
+    slideBack() {
+      const currentPositionValue = parseInt(this.currentPosition, 10);
+
+      if (currentPositionValue < 0) {
+        this.currentPosition = `${currentPositionValue + this.slideWidth}px`;
+      }
     },
 
     saveData(){
@@ -186,4 +211,35 @@ export default {
 };
 </script>
 
-			
+<style>
+.slider-container {
+  width: 400px;
+  height: 200px;
+  overflow: hidden;
+  position: relative;
+}
+
+.slider-content {
+  width: 400%;
+  height: 100%;
+  display: flex;
+  transition: left 0.5s;
+}
+
+.slide {
+  width: 25%;
+  height: 100%;
+  flex-shrink: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  background-color: #f0f0f0;
+  border: 1px solid #ccc;
+}
+
+.slide button {
+  margin-top: 10px;
+}
+</style>		
