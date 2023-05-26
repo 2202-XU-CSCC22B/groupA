2821cacb2a60c1fcba622b22a4c874abf1d3a800
Christianne Mikaela Ysabel F. San Jose <124657000+ChristianneSanJose@users.noreply.github.com>
2023-05-26 18:41:16 +0800

    Update server form.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const trackRoutes = require('./routes/trackRoutes');
+const formRoutes = require('./routes/formRoute');
 const PORT = 3000;
 
 mongoose.set("strictQuery", false);
@@ -35,6 +36,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+app.use('/form', formRoutes);
 app.use('/track', trackRoutes); // Use the trackRoutes for the /track endpoint
 
 
