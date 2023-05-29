b3ffdf04cca45dec8570b6e3fd660c5121ce4c84
Nicky Pojas <200520223@my.xu.edu.ph>
2023-05-30 01:35:36 +0800

    modified server and ui files for deployment

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.use(express.json());
 
 // Enable CORS headers
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.setHeader('Access-Control-Allow-Origin', 'https://ingress-egress.onrender.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
--- ui/src/boot/axios-config.js
+++ ui/src/boot/axios-config.js
@@ -2,7 +2,7 @@ import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/' // Replace with your Node server URL
+  baseURL: 'https://ingress-egress-server.onrender.com' // Replace with your Node server URL
 });
 
 export default boot(({ app }) => {
