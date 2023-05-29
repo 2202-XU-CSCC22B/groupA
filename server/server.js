cd346310ca5604f33794951fd3518fffa144f86c
Nicky Pojas <200520223@my.xu.edu.ph>
2023-05-30 02:03:50 +0800

    modified server and ui files for deployment

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.use(express.json());
 
 // Enable CORS headers
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://ingress-egress.onrender.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://groupa.onrender.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
